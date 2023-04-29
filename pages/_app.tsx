32999aa0aa75ec83c138e8772d2ec88beb6a6187
Tomas Dal Verme <tdalverme@hotmail.com>
2023-04-29 02:17:34 -0300

    refactor: added tailwind and removed mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MantineProvider } from '@mantine/core';
+import "../styles/globals.css";
 
 export default function App(props: AppProps) {
   const { Component, pageProps } = props;
@@ -15,16 +15,7 @@ export default function App(props: AppProps) {
         />
       </Head>
 
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: "light",
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
+      <Component {...pageProps} />
     </>
   );
 }
